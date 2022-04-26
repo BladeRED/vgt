a2c6f72c96f29739a0893a7b2c74e758d0d28343
BladeRED <juleray@msn.com>
2022-04-26 15:24:05 +0200

    squelette du site fait, début du stylisme en css

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -2,3 +2,14 @@
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+// CUSTOM JS HERE //
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+}
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
