004bd531e4347821692c33622939a636b6d92afd
BladeRED <juleray@msn.com>
2022-05-09 12:49:51 +0200

    On essaie de relier la bdd

---
+++ Models/User.php
@@ -0,0 +1,114 @@
+<?php
+
+class User
+
+{
+
+    private $id;
+    private $mail;
+    private $username;
+    private $password;
+    private $role;
+
+    /**
+     * @param $id
+     * @param $mail
+     * @param $username
+     * @param $password
+     * @param $role
+     */
+    public function __construct($id, $mail, $username, $password, $role)
+    {
+        $this->id = $id;
+        $this->mail = $mail;
+        $this->username = $username;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param mixed $mail
+     */
+    public function setMail($mail): void
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role): void
+    {
+        $this->role = $role;
+    }
+
+
+}
+
+
+
+?>;
